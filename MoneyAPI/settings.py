7f54b88d550a5ecaee264613eb5e6b062dbde722
jacktembo <jack@jacktembo.com>
2024-11-16 00:27:40 +0300

    ...

--- MoneyAPI/settings.py
+++ MoneyAPI/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-01ez(!+#-xt)_#$wihkjlj7r0%rn#oqt5h3vy2bo7qm(883r29'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*',]
 
