d48b1db2a287dd7f5c0bdf8af5854515c0e8ab5e
jacktembo <jack@jacktembo.com>
2024-11-16 11:20:58 +0300

    ...

--- MoneyAPI/urls.py
+++ MoneyAPI/urls.py
@@ -17,12 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .api import api
-from django.views.generic import RedirectView
 
 
 urlpatterns = [
-    path('/', admin.site.urls),
-    path('', RedirectView.as_view(url='/admin/', permanent=True)),
     path('admin/', admin.site.urls),
     path("api/", api.urls)
 ]
